aa2e85b9cb69a49ce98e8eb52552bd7afcbecdc6
graham <batzlerg@gmail.com>
2019-11-26 20:26:40 -0700

    add replace all transform, drastically simplify all transformation methods

--- src/constants.js
+++ src/constants.js
@@ -1,71 +1,43 @@
 const JS_TRANSFORMS = {
   'remove': (base, input) => {
-    let i = base.indexOf(input);
-    if (i < 0) {
-      return base;
-    }
-    return [
-      base.slice(0, i),
-      base.slice(i + input.length, base.length)
-    ].reduce((acc, curr) => acc + curr);
+    return base.replace(input, '');
   },
   'remove all': (base, input) => {
-    let temp = base;
-    while (temp !== JS_TRANSFORMS['remove'](temp, input)) {
-      temp = JS_TRANSFORMS['remove'](temp, input);
-    }
-    return temp;
+    return base.replace(new RegExp(input, 'g'), '');
   },
   'replace': (base, input, insert) => {
-    let i = base.indexOf(input);
-    if (i < 0) { return base; }
-    return [
-      base.slice(0, i),
-      insert,
-      base.slice(i + input.length, base.length)
-    ].reduce((acc, curr) => acc + curr);
+    return base.replace(input, insert);
+  },
+  'replace all': (base, input, insert) => {
+    return base.replace(new RegExp(input, 'g'), insert);
   },
   'add before': (base, input, insert) => {
-    let i = base.indexOf(input);
-    if (i < 0) { return base; }
-    return [
-      base.slice(0, i),
-      insert,
-      base.slice(i, base.length)
-    ].reduce((acc, curr) => acc + curr);
+    return base.replace(input, `${insert}${input}`);
   },
   'add after': (base, input, insert) => {
-    let i = base.indexOf(input);
-    if (i < 0) { return base; }
-    return [
-      base.slice(0, i + input.length),
-      insert,
-      base.slice(i + input.length, base.length)
-    ].reduce((acc, curr) => acc + curr);
+    return base.replace(input, `${input}${insert}`);
   }
 };
 
 const BASH_TRANSFORMS = {
-  'remove': input => ({
-    util: 'awk',
-    command: `sub(\"${input}\",\"\");`
-  }),
-  'remove all': input => ({
-    util: 'awk',
-    command: `gsub(\"${input}\",\"\");`
-  }),
-  'replace': (input, insert) => ({
-    util: 'awk',
-    command: `sub(\"${input}\",\"${insert}\");`
-  }),
-  'add before': (input, insert) => ({
-    util: 'awk',
-    command: `sub(\"${input}\",\"${insert}${input}\");`
-  }),
-  'add after': (input, insert) => ({
-    util: 'awk',
-    command: `sub(\"${input}\",\"${input}${insert}\");`
-  }),
+  'remove': input => {
+    return `sub(\"${input}\",\"\");`
+  },
+  'remove all': input => {
+    return `gsub(\"${input}\",\"\");`
+  },
+  'replace': (input, insert) => {
+    return `sub(\"${input}\",\"${insert}\");`
+  },
+  'replace all': (input, insert) => {
+    return `gsub(\"${input}\",\"${insert}\");`
+  },
+  'add before': (input, insert) => {
+    return `sub(\"${input}\",\"${insert}${input}\");`
+  },
+  'add after': (input, insert) => {
+    return `sub(\"${input}\",\"${input}${insert}\");`
+  },
 };
 
 export default {
@@ -73,11 +45,13 @@ export default {
     'remove',
     'remove all',
     'replace',
+    'replace all',
     'add before',
     'add after'
   ],
   INSERT_TRANSFORMS: [
     'replace',
+    'replace all',
     'add before',
     'add after'
   ],
