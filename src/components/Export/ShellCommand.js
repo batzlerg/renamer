3d02a6c82eaedd36bd827bed28c56ebec1a8a1ff
graham <batzlerg@gmail.com>
2019-11-26 20:57:04 -0700

    flesh out ShellCommand, add option for directory file renaming

--- src/components/Export/ShellCommand.js
+++ src/components/Export/ShellCommand.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CONSTS from '../../constants';
 import './ShellCommand.css';
 
 function ShellCommand(props) {
-  console.log(CONSTS);
+  const [ isDirectoryRename, setIsDirectoryRename ] = useState(false);
   let commands = [];
   // iterator declaration
-  if (props.renameDirectoryFiles) {
+  if (isDirectoryRename) {
     commands.push(`for f in [[directory]]*; do mv \"$f\" \"$(echo $f | `);
   }
   commands.push('awk \'{');
   // loop over transformations to pipe to sed
   for (let t of props.transformations) {
-    console.log(t);
     commands.push(CONSTS.BASH_TRANSFORMS[t.type](
       t.text,
       t.insert || '' // todo: change to ||
-    ).command);
+    ));
   }
   commands.push('}1\'')
-  if (props.renameDirectoryFiles) {
+  if (isDirectoryRename) {
     commands.push(')\"; done');
   }
 
   return (
-    <div className='shellCommand'>
-      <p>{ commands.reduce((acc, curr) => `${acc} ${curr}`) }</p>
-    </div>
+    <>
+      <div className="header">
+        <h2>Shell Output</h2>
+        <p>Copy/paste to any BASH shell to perform transformations</p>
+      </div>
+      <div className="directoryCheckboxWrapper">
+        <input
+          type="checkbox"
+          name="isDirectoryRename"
+          id="isDirectoryRename"
+          className="directoryCheckbox"
+          checked={isDirectoryRename}
+          onChange={e => setIsDirectoryRename(e.target.checked)}
+        />
+        <label htmlFor="isDirectoryRename">Rename files in a directory</label>
+      </div>
+      <div className='shellCommand'>
+        <p>{ commands.reduce((acc, curr) => `${acc} ${curr}`) }</p>
+      </div>
+    </>
   );
 }
 
